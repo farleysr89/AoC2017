e136664a9cab90877d70462104caaf0fa388de62
Farley <33631912+farleysr89@users.noreply.github.com>
2021-01-11 18:17:12 -0600

    Day 02 Part 1 Complete

---
+++ Day02/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day02
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            SolvePart1();
+            SolvePart2();
+        }
+
+        private static void SolvePart1()
+        {
+            var input = File.ReadAllText("Input.txt");
+            var data = input.Split('\n').Select(s => s.Replace('\t', ' ')).ToList();
+            var checkSum = data.Where(s => s != "").Select(s => s.Split(" ").Select(int.Parse)).Select(parts => parts.Max() - parts.Min()).Sum();
+            Console.WriteLine("CheckSum = " + checkSum);
+        }
+
+        private static void SolvePart2()
+        {
+            var input = File.ReadAllText("Input.txt");
+            var data = input.Split('\n').ToList();
+            Console.WriteLine("");
+        }
+    }
+}
