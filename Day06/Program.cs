b27cb4bd32d3abf6b201cb36af37332deca56f26
Farley <33631912+farleysr89@users.noreply.github.com>
2021-01-11 21:22:05 -0600

    Day 06 Part 1 Complete

--- Day06/Program.cs
+++ Day06/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -15,8 +16,28 @@ namespace Day06
         private static void SolvePart1()
         {
             var input = File.ReadAllText("Input.txt");
-            var data = input.Split('\n').ToList();
-            Console.WriteLine("");
+            var data = input.Split('\n')[0].Replace('\t', ' ').Split(" ").Select(int.Parse).ToList();
+            var states = new HashSet<string> { string.Join("-", data.ToArray()) };
+            var iterationCount = 0;
+            while (true)
+            {
+                iterationCount++;
+                var max = data.Max(b => b);
+                var maxIndex = data.FindIndex(b => b == max);
+                data[maxIndex] = 0;
+                while (max > 0)
+                {
+                    max--;
+                    maxIndex++;
+                    if (maxIndex == data.Count) maxIndex = 0;
+                    data[maxIndex]++;
+                }
+                var s = string.Join("-", data.ToArray());
+                if (states.Contains(s)) break;
+                states.Add(string.Join("-", data.ToArray()));
+            }
+
+            Console.WriteLine("Iteration Count = " + iterationCount);
         }
 
         private static void SolvePart2()
