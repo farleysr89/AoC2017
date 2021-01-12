f2b488e68a49e19fa072f9b9fbd14ebbd6e097a5
Farley <33631912+farleysr89@users.noreply.github.com>
2021-01-11 21:43:44 -0600

    Day 06 Part 2 Complete

--- Day06/Program.cs
+++ Day06/Program.cs
@@ -43,8 +43,47 @@ namespace Day06
         private static void SolvePart2()
         {
             var input = File.ReadAllText("Input.txt");
-            var data = input.Split('\n').ToList();
-            Console.WriteLine("");
+            var data = input.Split('\n')[0].Replace('\t', ' ').Split(" ").Select(int.Parse).ToList();
+            var states = new HashSet<string> { string.Join("-", data.ToArray()) };
+            var iterationCount = 0;
+            while (true)
+            {
+                iterationCount++;
+                var max = data.Max(b => b);
+                var maxIndex = data.FindIndex(b => b == max);
+                data[maxIndex] = 0;
+                while (max > 0)
+                {
+                    max--;
+                    maxIndex++;
+                    if (maxIndex == data.Count) maxIndex = 0;
+                    data[maxIndex]++;
+                }
+                var s = string.Join("-", data.ToArray());
+                if (states.Contains(s)) break;
+                states.Add(string.Join("-", data.ToArray()));
+            }
+
+            var state = string.Join("-", data.ToArray());
+            iterationCount = 0;
+            while (true)
+            {
+                iterationCount++;
+                var max = data.Max(b => b);
+                var maxIndex = data.FindIndex(b => b == max);
+                data[maxIndex] = 0;
+                while (max > 0)
+                {
+                    max--;
+                    maxIndex++;
+                    if (maxIndex == data.Count) maxIndex = 0;
+                    data[maxIndex]++;
+                }
+                var s = string.Join("-", data.ToArray());
+                if (s == state) break;
+            }
+
+            Console.WriteLine("Iteration Count = " + iterationCount);
         }
     }
 }
