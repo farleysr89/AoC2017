04346d17663bba50f6bcb50671e44f2932bc2ddd
Farley <33631912+farleysr89@users.noreply.github.com>
2021-01-11 18:50:21 -0600

    Day 03 Part 1 Complete

--- Day03/Program.cs
+++ Day03/Program.cs
@@ -15,8 +15,15 @@ namespace Day03
         private static void SolvePart1()
         {
             var input = File.ReadAllText("Input.txt");
-            var data = input.Split('\n').ToList();
-            Console.WriteLine("");
+            var data = int.Parse(input);
+            var i = 1;
+            while (Math.Pow(i, 2) < data)
+            {
+                i++;
+            }
+
+            var solution = i - 1 - (Math.Pow(i, 2) - data);
+            Console.WriteLine("Answer = " + solution);
         }
 
         private static void SolvePart2()
