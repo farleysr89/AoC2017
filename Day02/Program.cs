5055be85221f7242c58c8914584f5dc124cd2c67
Farley <33631912+farleysr89@users.noreply.github.com>
2021-01-11 18:26:35 -0600

    Day 02 Part 2 Complete

--- Day02/Program.cs
+++ Day02/Program.cs
@@ -23,8 +23,35 @@ namespace Day02
         private static void SolvePart2()
         {
             var input = File.ReadAllText("Input.txt");
-            var data = input.Split('\n').ToList();
-            Console.WriteLine("");
+            var data = input.Split('\n').Select(s => s.Replace('\t', ' ')).ToList();
+            var checkSum = 0;
+            foreach (var s in data.Where(s => s != ""))
+            {
+                var parts = s.Split(" ").Select(int.Parse);
+                var found = false;
+                foreach (var x in parts)
+                {
+                    foreach (var y in parts.Where(y => y != x))
+                    {
+                        if (x % y == 0)
+                        {
+                            found = true;
+                            checkSum += x / y;
+                            break;
+                        }
+
+                        if (y % x == 0)
+                        {
+                            found = true;
+                            checkSum += y / x;
+                            break;
+                        }
+                    }
+
+                    if (found) break;
+                }
+            }
+            Console.WriteLine("CheckSum = " + checkSum);
         }
     }
 }
