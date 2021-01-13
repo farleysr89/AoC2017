bf8be9071f685342a661f070ad0b0fa71392c546
Farley <33631912+farleysr89@users.noreply.github.com>
2021-01-12 19:28:43 -0600

    Day 09 Part 1 Complete

--- Day09/Program.cs
+++ Day09/Program.cs
@@ -16,7 +16,43 @@ namespace Day09
         {
             var input = File.ReadAllText("Input.txt");
             var data = input.Split('\n').ToList();
-            Console.WriteLine("");
+            var stream = data[0];
+            var score = 0;
+            var groupCount = 0;
+            var ignore = false;
+            for (var i = 0; i < stream.Length; i++)
+            {
+                var c = stream[i];
+                if (!ignore)
+                {
+                    if (c == '{')
+                    {
+                        groupCount++;
+                        score += groupCount;
+                    }
+                    else if (c == '}')
+                    {
+                        groupCount--;
+                    }
+                    else if (c == '<')
+                    {
+                        ignore = true;
+                    }
+                    else if (c == '!')
+                    {
+                        i++;
+                    }
+                }
+                else if (c == '>')
+                {
+                    ignore = false;
+                }
+                else if (c == '!')
+                {
+                    i++;
+                }
+            }
+            Console.WriteLine("Total score = " + score);
         }
 
         private static void SolvePart2()
