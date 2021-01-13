506af601ba86de17c01eca85251eb351e50d6775
Farley <33631912+farleysr89@users.noreply.github.com>
2021-01-12 22:40:12 -0600

    Day 12 Part 1 Complete

---
+++ Day12/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day12
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            SolvePart1();
+            SolvePart2();
+        }
+
+        private static void SolvePart1()
+        {
+            var input = File.ReadAllText("Input.txt");
+            var data = input.Split('\n').ToList();
+            var connections = new List<string> { "0" };
+            var change = true;
+            var tmpData = new List<string>(data);
+            while (change)
+            {
+                change = false;
+                foreach (var s in tmpData.Where(s => s != ""))
+                {
+                    var parts = s.Split(" <-> ");
+                    if (connections.Contains(parts[0]))
+                    {
+                        change = true;
+                        var newNums = parts[1].Split(", ");
+                        connections.AddRange(newNums.Where(y => !connections.Contains(y)).Select(x => x));
+                        data.Remove(s);
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                tmpData = new List<string>(data);
+                data = new List<string>(tmpData);
+            }
+
+            Console.WriteLine("Solution = " + connections.Count);
+        }
+
+        private static void SolvePart2()
+        {
+            var input = File.ReadAllText("Input.txt");
+            var data = input.Split('\n').ToList();
+            Console.WriteLine("");
+        }
+    }
+}
