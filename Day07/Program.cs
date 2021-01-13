23184c9f97d7bcb969b0fa128388e7bdef4d1cd6
Farley <33631912+farleysr89@users.noreply.github.com>
2021-01-12 18:42:01 -0600

    Day 07 Part 2 Complete

--- Day07/Program.cs
+++ Day07/Program.cs
@@ -47,8 +47,46 @@ namespace Day07
         private static void SolvePart2()
         {
             var input = File.ReadAllText("Input.txt");
-            var data = input.Split('\n').ToList();
-            Console.WriteLine("");
+            var data = input.Split('\n').Where(s => s != "").OrderBy(s => s.Length).ToList();
+            var nodes = new List<Node>();
+            var tmpData = new List<string>(data);
+            foreach (var s in tmpData.Where(s => !s.Contains("->")))
+            {
+                nodes.Add(new Node { Name = s.Split(" ")[0], Weight = int.Parse(s.Split(" ")[1].Replace("(", "").Replace(")", "")) });
+                data.Remove(s);
+            }
+            tmpData = new List<string>(data);
+            while (data.Count > 0)
+            {
+                foreach (var s in tmpData)
+                {
+                    var parts = s.Split(" -> ");
+                    var children = parts[1].Split(", ");
+                    if (children.Any(c => nodes.All(n => n.Name != c))) continue;
+                    nodes.Add(new Node { Name = parts[0].Split(" ")[0], Children = nodes.Where(n => children.Contains(n.Name)).ToList(), Weight = int.Parse(parts[0].Split(" ")[1].Replace("(", "").Replace(")", "")) });
+                    foreach (var c in nodes.Where(n => children.Contains(n.Name)))
+                    {
+                        c.Parent = nodes.First(x => x.Name == parts[0].Split(" ")[0]);
+                    }
+                    data.Remove(s);
+                }
+                tmpData = new List<string>(data);
+            }
+
+            var problemNodes = nodes.Where(n =>
+                n.Children.Any(c => c.NestedWeight() != n.Children.Max(cc => cc.NestedWeight())));
+            var node = problemNodes.First();
+            //Console.WriteLine(c.Name + " " + string.Join(",", c.Children.Select(x => x.Name + ":" + x.NestedWeight())));
+            var p = node.Children.GroupBy(c => c.NestedWeight()).First(g => g.Count() == 1).First();
+            var g = node.Children.GroupBy(c => c.NestedWeight()).First(g => g.Count() > 1).First().NestedWeight();
+            var t = Calculate(g, p);
+            Console.WriteLine("Final weight of changed node = " + t);
+
+        }
+
+        private static int Calculate(int target, Node n)
+        {
+            return target - n.Children.Sum(c => c.NestedWeight());
         }
     }
 
@@ -56,6 +94,11 @@ namespace Day07
     {
         internal string Name;
         internal Node Parent;
-        internal List<Node> Children;
+        internal List<Node> Children = new List<Node>();
+        internal int Weight;
+        internal int NestedWeight()
+        {
+            return Weight + Children.Select(c => c.NestedWeight()).Sum();
+        }
     }
 }
