50d54241e98b4dc50754d45619b5ace871d97ebf
Farley <33631912+farleysr89@users.noreply.github.com>
2021-01-12 21:31:18 -0600

    Day 11 Part 2 Complete

--- Day11/Program.cs
+++ Day11/Program.cs
@@ -52,14 +52,53 @@ namespace Day11
                         break;
                 }
             }
-            Console.WriteLine("Distance to child is " + ((Math.Abs(x) + Math.Abs(y) + Math.Abs(z)) / 2));
+            Console.WriteLine("Distance to child is " + (Math.Abs(x) + Math.Abs(y) + Math.Abs(z)) / 2);
         }
 
         private static void SolvePart2()
         {
+            //Big help from here: https://www.redblobgames.com/grids/hexagons/
             var input = File.ReadAllText("Input.txt");
             var data = input.Split('\n').ToList();
-            Console.WriteLine("");
+            var moves = data[0].Split(",");
+            int x = 0, y = 0, z = 0;
+            var maxDistance = int.MinValue;
+            foreach (var m in moves)
+            {
+                switch (m)
+                {
+                    case "nw":
+                        x--;
+                        y++;
+                        break;
+                    case "n":
+                        y++;
+                        z--;
+                        break;
+                    case "ne":
+                        x++;
+                        z--;
+                        break;
+                    case "se":
+                        x++;
+                        y--;
+                        break;
+                    case "s":
+                        y--;
+                        z++;
+                        break;
+                    case "sw":
+                        x--;
+                        z++;
+                        break;
+                    default:
+                        Console.WriteLine("Something Broke!");
+                        break;
+                }
+
+                maxDistance = Math.Max(maxDistance, (Math.Abs(x) + Math.Abs(y) + Math.Abs(z)) / 2);
+            }
+            Console.WriteLine("Max Distance to child is " + maxDistance);
         }
     }
 }
