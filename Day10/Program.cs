40bb95703c04c73636e70877eda127f8e33250e1
Farley <33631912+farleysr89@users.noreply.github.com>
2021-01-12 20:29:23 -0600

    Day 10 Part 1 Complete

--- Day10/Program.cs
+++ Day10/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -16,7 +17,34 @@ namespace Day10
         {
             var input = File.ReadAllText("Input.txt");
             var data = input.Split('\n').ToList();
-            Console.WriteLine("");
+            var lengths = data[0].Split(",").Select(int.Parse);
+            var skip = 0;
+            var currPosition = 0;
+            var nums = new List<int>();
+            for (var i = 0; i <= 255; i++)
+            {
+                nums.Add(i);
+            }
+
+            foreach (var l in lengths)
+            {
+                var tmpArray = new int[l];
+                for (var i = 0; i < l; i++)
+                {
+                    tmpArray[i] = nums[(currPosition + i) % nums.Count];
+                }
+
+                tmpArray = tmpArray.Reverse().ToArray();
+                for (var i = 0; i < l; i++)
+                {
+                    nums[(currPosition + i) % nums.Count] = tmpArray[i];
+                }
+                currPosition += l + skip;
+                currPosition %= nums.Count;
+                skip++;
+            }
+
+            Console.WriteLine("Solution is " + nums[0] * nums[1]);
         }
 
         private static void SolvePart2()
