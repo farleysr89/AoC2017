b05e2710195d05868f69325e44b38f600d1769f6
Farley <33631912+farleysr89@users.noreply.github.com>
2021-01-14 18:55:54 -0600

    Day 16 Part 2 Complete

--- Day16/Program.cs
+++ Day16/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -55,8 +56,87 @@ namespace Day16
         private static void SolvePart2()
         {
             var input = File.ReadAllText("Input.txt");
-            var data = input.Split('\n').ToList();
-            Console.WriteLine("");
+            var data = input.Split(',').ToList();
+            var programs = new StringBuilder("abcdefghijklmnop");
+            var appeared = new HashSet<string> { programs.ToString() };
+            var loop = 0;
+            for (var i = 0; i < 1000000000; i++)
+            {
+                foreach (var s in data)
+                {
+                    switch (s[0])
+                    {
+                        case 's':
+                            var moves = int.Parse(s[1..]);
+                            programs = new StringBuilder(ShiftRight(programs.ToString(), moves));
+                            break;
+                        case 'x':
+                            var sx = s[1..];
+                            var sxParts = sx.Split("/").Select(int.Parse).ToList();
+                            var tmpA = programs[sxParts[0]];
+                            var tmpB = programs[sxParts[1]];
+                            programs[sxParts[0]] = tmpB;
+                            programs[sxParts[1]] = tmpA;
+                            break;
+                        case 'p':
+                            var sp = s[1..];
+                            var spParts = sp.Split("/").ToList();
+                            var tmpC = spParts[0][0];
+                            var tmpD = spParts[1][0];
+                            var tmpE = programs.ToString().IndexOf(tmpC);
+                            var tmpF = programs.ToString().IndexOf(tmpD);
+                            programs[tmpE] = tmpD;
+                            programs[tmpF] = tmpC;
+                            break;
+                        default:
+                            Console.WriteLine("Something Broke!");
+                            break;
+                    }
+                }
+
+                if (appeared.Contains(programs.ToString()))
+                {
+                    loop = i + 1;
+                    break;
+                }
+                else appeared.Add(programs.ToString());
+            }
+            for (var i = 0; i < 1000000000 % loop; i++)
+            {
+                foreach (var s in data)
+                {
+                    switch (s[0])
+                    {
+                        case 's':
+                            var moves = int.Parse(s[1..]);
+                            programs = new StringBuilder(ShiftRight(programs.ToString(), moves));
+                            break;
+                        case 'x':
+                            var sx = s[1..];
+                            var sxParts = sx.Split("/").Select(int.Parse).ToList();
+                            var tmpA = programs[sxParts[0]];
+                            var tmpB = programs[sxParts[1]];
+                            programs[sxParts[0]] = tmpB;
+                            programs[sxParts[1]] = tmpA;
+                            break;
+                        case 'p':
+                            var sp = s[1..];
+                            var spParts = sp.Split("/").ToList();
+                            var tmpC = spParts[0][0];
+                            var tmpD = spParts[1][0];
+                            var tmpE = programs.ToString().IndexOf(tmpC);
+                            var tmpF = programs.ToString().IndexOf(tmpD);
+                            programs[tmpE] = tmpD;
+                            programs[tmpF] = tmpC;
+                            break;
+                        default:
+                            Console.WriteLine("Something Broke!");
+                            break;
+                    }
+                }
+            }
+
+            Console.WriteLine("Final order = " + programs);
         }
 
         public static string ShiftRight(string s, int count)
