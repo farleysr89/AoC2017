7a3f869f0f419f4c991e89d0543c4866a9f1517a
Farley <33631912+farleysr89@users.noreply.github.com>
2021-01-14 19:30:11 -0600

    Day 17 Part 1 Complete

---
+++ Day17/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day17
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            SolvePart1();
+            SolvePart2();
+        }
+
+        private static void SolvePart1()
+        {
+            var input = File.ReadAllText("Input.txt");
+            var data = int.Parse(input);
+            var output = new List<int> { 0 };
+            var position = 0;
+            for (var i = 1; i <= 2017; i++)
+            {
+                position = (position + data) % output.Count;
+                output.Insert(position, i);
+                position++;
+            }
+            Console.WriteLine("Next num is " + output[output.IndexOf(2017) + 1]);
+        }
+
+        private static void SolvePart2()
+        {
+            var input = File.ReadAllText("Input.txt");
+            var data = input.Split('\n').ToList();
+            Console.WriteLine("");
+        }
+    }
+}
