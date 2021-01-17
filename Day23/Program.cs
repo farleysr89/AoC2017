fa28ffa599c66069f5c1d85e7b2bf99269efcb1d
Farley <33631912+farleysr89@users.noreply.github.com>
2021-01-17 08:31:38 -0600

    Day 23 Part 2 Complete

--- Day23/Program.cs
+++ Day23/Program.cs
@@ -65,9 +65,31 @@ namespace Day23
 
         private static void SolvePart2()
         {
-            var input = File.ReadAllText("Input.txt");
-            var data = input.Split('\n').ToList();
-            Console.WriteLine("");
+            const int b = 105700;
+            const int c = 122700;
+            var h = 0;
+            for (var i = b; i <= c; i += 17)
+            {
+                if (!IsPrime(i)) h++;
+            }
+
+
+            Console.WriteLine("Value of register h = " + h);
+        }
+        //Function taken from here: https://stackoverflow.com/a/15743238
+        private static bool IsPrime(int number)
+        {
+            if (number <= 1) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
+
+            var boundary = (int)Math.Floor(Math.Sqrt(number));
+
+            for (var i = 3; i <= boundary; i += 2)
+                if (number % i == 0)
+                    return false;
+
+            return true;
         }
     }
 }
